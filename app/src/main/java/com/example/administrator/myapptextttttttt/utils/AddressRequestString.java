46ffd72b50a0e2cf9c5313873129795b81edc53d
Liuxiaowei <1457810453@qq.com>
2018-07-02 11:54:14 +0800

    测试一下111

--- app/src/main/java/com/example/administrator/myapptextttttttt/utils/AddressRequestString.java
+++ app/src/main/java/com/example/administrator/myapptextttttttt/utils/AddressRequestString.java
@@ -5,6 +5,7 @@ package com.example.administrator.myapptextttttttt.utils;
  */
 
 public class AddressRequestString {
+
     public static final String BASE_URL = "https://api.douban.com/v2/movie/";
 
     public static final String TEXT = "http://58.42.248.105:1588/safe.php/AppApi/w_login";
